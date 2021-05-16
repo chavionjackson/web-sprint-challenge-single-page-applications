6933dde2ad0b36fefa96cbb2abba295a40c35bbe
Chevy <75184977+chavionjackson@users.noreply.github.com>
2021-05-16 05:08:45 -0700

    Broken cypress

---
+++ cypress/integration/examples/Form_test.js
@@ -0,0 +1,22 @@
+import cy from 'cypress';
+
+describe('Pizza App', () => {
+
+    beforeEach(() => cy.visit('http://localhost:3001'))
+    describe('typing into box, selecting toppings, and submit', () => {
+        it('can type and see if the correct name', () => {
+            cy.get('input[name=name]')
+             .type('mickey mouse')
+             .should('have.value', 'mickey mouse')          
+            cy.get('input[name=textarea]')
+            .click()
+            cy.get('#submit-btn')
+            .click()
+        })
+    })
+    describe('Filling out inputs and cancelling', () => {
+        it('submit button is disabled', () => {
+            cy.get('#submit-btn').should('be.disabled')
+        })
+    })
+})
